5c5dc8296d238986e90fdbd468951786c906be3f
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 17:06:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,4 +11,16 @@
   
  end
  
+ def my_collect(collection)
+   i = 0
+   empty_array = []
+   
+  while i < collection.length
+    empty_array << yield(collection[i].split(" ").first)   
+    i += 1
+  end 
+  
+  empty_array
+  
+ end
 
