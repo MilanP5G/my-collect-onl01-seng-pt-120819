a93d2e3617863de1fc148a963f0d868c248e882e
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 16:52:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,12 @@
    i = 0
    empty_array = []
    
-  while i < array.length
-  yield array[i]
+  while i < collection.length
+  empty_array << yield array[i]
     i += 1
   end 
+  
+  empty_array
   
  end
  
